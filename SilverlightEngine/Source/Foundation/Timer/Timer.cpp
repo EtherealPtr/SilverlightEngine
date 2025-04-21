2ab1bbfb74f620661b8a9718bd0191efa6f85d8d
Rony Hanna <Rony.Hanna@outlook.com>
2025-04-21 21:40:51 +1200

    Add functionality to measure time duration in ms for performance debugging

--- SilverlightEngine/Source/Foundation/Timer/Timer.cpp
+++ SilverlightEngine/Source/Foundation/Timer/Timer.cpp
@@ -4,55 +4,29 @@
 namespace Silverlight
 {
     Timer::Timer() noexcept :
-        m_LastTime{ clock_t::now() },
-        m_StartTime{ clock_t::now() },
+        m_LastTime{ std::chrono::steady_clock::now() },
+        m_StartTime{ std::chrono::steady_clock::now() },
         m_DeltaTime{ 0.0 },
-        m_ElapsedTime{ 0.0 },
-        m_FrameCount{ 0 },
-        m_Fps{ 0 },
-        m_TimerRunning{ false },
-        m_FpsDisplayEnabled{ false }
+        m_ElapsedTime{ 0.0 }
     {}
 
 	void Timer::Update()
 	{
-        const auto currentTime{ clock_t::now() };
+        const auto currentTime{ std::chrono::steady_clock::now() };
         const std::chrono::duration<double> delta{ currentTime - m_LastTime };
         m_DeltaTime = delta.count();
         m_LastTime = currentTime;
-
-        if (!m_FpsDisplayEnabled)
-        {
-            return;
-        }
-
-        ++m_FrameCount;
-        m_ElapsedTime += m_DeltaTime;
-
-        if (m_ElapsedTime >= 1.0)
-        {
-            m_Fps = m_FrameCount;
-            m_FrameCount = 0;
-            m_ElapsedTime = 0.0;
-            SE_LOG(LogCategory::Trace, "FPS: %d", m_Fps);
-        }
 	}
 
     void Timer::StartTimer() noexcept
     {
-        m_TimerRunning = true;
-        m_StartTime = clock_t::now();
+        m_StartTime = std::chrono::steady_clock::now();
     }
 
     void Timer::StopTimer()
     {
-        if (!m_TimerRunning)
-		{
-			return;
-		}
-
-        const auto elapsedTime{ std::chrono::duration_cast<std::chrono::milliseconds>(clock_t::now() - m_StartTime).count() };
-        SE_LOG(LogCategory::Trace, "[Elapsed time]: %d", elapsedTime);
-        m_TimerRunning = false;
+        const auto currentTime{ std::chrono::steady_clock::now() };
+        const double frameTimeMs{ std::chrono::duration<double, std::milli>(currentTime - m_StartTime).count() };
+        SE_LOG(LogCategory::Trace, "[Elapsed time]: %.3f ms", frameTimeMs);
     }
 } // End of namespace
--- SilverlightEngine/Source/Foundation/Timer/Timer.h
+++ SilverlightEngine/Source/Foundation/Timer/Timer.h
@@ -11,27 +11,21 @@ namespace Silverlight
 		Timer() noexcept;
 
 		void Update();
-		void ToggleFpsDisplay(const bool _toggle) noexcept { m_FpsDisplayEnabled = _toggle; }
-		double GetDeltaTime() const noexcept { return m_DeltaTime; }
 		void StartTimer() noexcept;
 		void StopTimer();
+		double GetDeltaTime() const noexcept { return m_DeltaTime; }
 
 		Timer(const Timer&) = delete;
-		Timer(Timer&&) = delete;
 		void operator=(const Timer&) = delete;
+		Timer(Timer&&) = delete;
 		void operator=(Timer&&) = delete;
 
 	private:
-		using clock_t = std::chrono::steady_clock;
-		using time_point_t = std::chrono::time_point<clock_t>;
+		using TimePoint = std::chrono::time_point<std::chrono::steady_clock>;
 
-		time_point_t m_LastTime;
-		time_point_t m_StartTime;
+		TimePoint m_LastTime;
+		TimePoint m_StartTime;
 		double m_DeltaTime;
 		double m_ElapsedTime;
-		int32 m_FrameCount;
-		int32 m_Fps;
-		bool m_TimerRunning;
-		bool m_FpsDisplayEnabled;
 	};
 } // End of namespace
